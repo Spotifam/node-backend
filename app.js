1bac4b13db59a9ed470d07efec1a0538ff04e9be
Blakemcw <blakecmcwilliam@gmail.com>
2019-11-20 22:57:39 -0800

    Add handling for pre-flight CORS requests

--- app.js
+++ app.js
@@ -15,7 +15,19 @@ app.use(cors());
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  
+  //intercepts OPTIONS method
+  if ('OPTIONS' === req.method) {
+    //respond with 200
+    res.send(200);
+  }
+  else {
+  //move on
+    next();
+  }
+
   next();
 });
 
