7c52a66ff1ad35aec29304068e78c697e61bc50d
Blakemcw <blakecmcwilliam@gmail.com>
2019-11-20 21:17:14 -0800

    Debug addsong endpoint

--- app.js
+++ app.js
@@ -20,9 +20,7 @@ app.use(function(req, res, next) {
 });
 
 // For POST requests
-var bodyParser = require("body-parser");
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
