52f0574ee0b5633fec09934ae9fcaac09b235dfc
Blakemcw <blakecmcwilliam@gmail.com>
2019-11-21 00:38:49 -0800

    Try with bodyParser

--- app.js
+++ app.js
@@ -3,14 +3,13 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var bodyParser = require('body-parser');
 
-var app = express();
-
-// For POST requests
-app.use(express.json());
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+
+var app = express();
 
 // CORS
 var cors = require('cors');
@@ -18,10 +17,12 @@ app.use(cors());
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next()
 });
+
+// For POST requests
+app.use(bodyParser.json({ type: "*/*" }));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
