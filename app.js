7b3054cb78bee5b3ed8a421208327d9728966949
Blakemcw <blakecmcwilliam@gmail.com>
2019-11-20 23:27:17 -0800

    Move parser above routes

--- app.js
+++ app.js
@@ -4,10 +4,13 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var app = express();
+
+// For POST requests
+app.use(express.json());
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
-var app = express();
 
 // CORS
 var cors = require('cors');
@@ -17,20 +20,8 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  
-  //intercepts OPTIONS method
-  if ('OPTIONS' === req.method) {
-    //respond with 200
-    res.send(200);
-  }
-  else {
-  //move on
-    next();
-  }
+  next()
 });
-
-// For POST requests
-app.use(express.json());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
